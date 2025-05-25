ed49fbab4d9facf3800dec603cfd856ea9534b79
sebastian_andreoletti <sebaeze@gmail.com>
2025-05-24 22:33:16 -0300

    ss

--- 00_langchain_basic_ollama/index.ts
+++ 00_langchain_basic_ollama/index.ts
@@ -9,12 +9,20 @@ const ollamaModel = new ChatOllama({
     model: process.env.MOLLAMA_MODEL||"orieg/gemma3-tools:1b",
     verbose: true
 });
-//
+/*
+*  3 ways to call the AI Model in Ollama: invoke, batch, stream
+*/
 async function main() {
-    const response = await ollamaModel.invoke(
-        "Which is the capital of France?"
-    );
-    console.log(response);
+    const respInvoke = await ollamaModel.invoke( "Which is the capital of France?" );
+    console.log("\nrespInvoke: ",respInvoke,"\n\n");
+
+    const respBatch = await ollamaModel.batch( ["Which is the capital of France?", "Describe the capital of France"] );
+    console.log("respBatch: ",respBatch,"\n\n");
+
+    const respStream = await ollamaModel.stream( "Which is the capital of France?" );
+    for await ( const chunk of respStream ){
+        console.log("chunk: ",chunk);
+    }
 };
 //
 main();
